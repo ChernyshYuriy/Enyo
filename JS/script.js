da52b13f430e3c1f8bf6db41a05db960b41dbd2e
ChernyshYuriy <yuriy.chernysh97@gmail.com>
2020-02-13 21:44:34 +0200

    1.1

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 const header_items = document.querySelectorAll(".item_animated");
 const header_loga = document.querySelector(".header_loga_animated");
 const article1__button = document.querySelector(".article1__button");
-const header_button = document.querySelector(".header_button");
+const header_button = document.getElementsByTagName("button");
 const navbar_collapse = document.querySelector(".navbar-collapse");
 
 const tl = new TimelineMax({});
@@ -15,7 +15,7 @@ const tl = new TimelineMax({});
      },500);
    });
  if($(window).width()>975){
-  tl.fromTo(header_items[0], 1,   {opacity: 0, y:-100},{opacity: 1, y: 0})
+  tl.fromTo(header_items[0], 1,   {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=0.5")
     .fromTo(header_items[1], 1.2, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=0.9")
     .fromTo(header_items[2], 1.2, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=1")
     .fromTo(header_items[3], 1.4, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=1")
@@ -23,19 +23,19 @@ const tl = new TimelineMax({});
  };
  setTimeout(function() {
       tl.fromTo(header_loga, 1,{opacity: 0},{opacity: 1})
-    .fromTo($(".header_button"),1.5,{opacity:0, x: "50"},{opacity:1, x: "0"})
-    .fromTo($(".article1__baner_text"), 1,{opacity:0, x: "-180"},{opacity:1, x: "0"})
-    .fromTo($(".article1__text"), 1,{opacity:0, x: "180"},{opacity:1, x: "0"})
-    .fromTo($(".article1__button"),1, {opacity:0, y:50},{opacity:1, y:0});
+    .fromTo($(".header_button"),        1.5,{opacity:0, x: "50"},{opacity:1, x: "0"}, "-=1.0")
+    .fromTo($(".article1__baner_text"), 1,{opacity:0, x: "-180"},{opacity:1, x: "0"}, "-=0.5")
+    .fromTo($(".article1__text"),       1,{opacity:0},{opacity:1}, "-=0.5")
+    .fromTo($(".article1__button"),     1,{opacity:0, y:50},{opacity:1, y:0},         "-=0.5");
  }, 100);
 
 header_button.addEventListener("click", function(){
   if(navbar_collapse.classList.contains("show")==false){
-    tl.fromTo(header_items[0], 0.4,  {opacity: 0, y:-100},{opacity: 1, y: 0})
-    .fromTo(header_items[1], 0.4, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=0.2")
-    .fromTo(header_items[2], 0.4, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=0.2")
-    .fromTo(header_items[3], 0.4, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=0.2")
-    .fromTo(header_items[4], 0.4, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=0.2");
+    tl.fromTo(header_items[0],0.4, {opacity: 0, y:-100},{opacity: 1, y: 0})
+    .fromTo(header_items[1],  0.4, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=0.2")
+    .fromTo(header_items[2],  0.4, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=0.2")
+    .fromTo(header_items[3],  0.4, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=0.2")
+    .fromTo(header_items[4],  0.4, {opacity: 0, y:-100},{opacity: 1, y: 0}, "-=0.2");
    }
    else if(navbar_collapse.classList.contains("show")==true){
   }
